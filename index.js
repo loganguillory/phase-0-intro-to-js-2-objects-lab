2543c321d6a3709f11f4c7ee4cdab02e7a318988
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-18 17:29:35 -0400

    commit tree f967b057643ec42d5f26f518738a3fc4cdae1618

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution in this file!
+// Write your solution in this file!new
+// const employee = {
+//     streetAddress: "",
+//     name: "" ,
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newObj = {...employee}
+//     newObj[key] = value
+//     return newObj
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newObj = {...employee}
+//     delete newObj[key];
+//     return newObj
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key];
+//     return employee;
+
+// };
+
+const employee = {
+    name: " ",
+    streetAddress: " "
+ }
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee}
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
